0bde80d22cae2cac2273c1b6a5b53431bd6dd074
Hippolyte Hilgers <hippolytehilgers@MacBook-Air-de-Hippolyte.local>
2021-03-20 18:36:49 +0100

    Scripts_Prefabs

--- Assets/Scripts/Trig.cs
+++ Assets/Scripts/Trig.cs
@@ -16,7 +16,7 @@ public class Trig : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        managerRoom1 = GameObject.Find("ManagerRoom1").GetComponent<ManagerRoom1>();
+        managerRoom1 = GameObject.Find("MANAGER_ROOM1").GetComponent<ManagerRoom1>();
         state = managerRoom1.GetState(id);
         UpdateColor();
     }
