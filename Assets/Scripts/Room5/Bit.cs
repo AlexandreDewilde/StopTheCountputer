d6c9c1fd846e0e31c3a87b1763b1e011afbf279a
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2021-03-21 11:34:21 +0100

    Add scripts for room 5

---
+++ Assets/Scripts/Room5/Bit.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bit : MonoBehaviour
+{
+    public float lifeTime;
+    public ManagerRoom5 gameManager;
+    private bool isGood;
+    private float initTime;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGood = Random.Range(0,2) != 0;
+        initTime = Time.time;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (isGood) spriteRenderer.color = new Color(0,255,0);
+        else spriteRenderer.color = new Color(255, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - initTime > lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameManager.ApplyCollider(isGood);
+        }
+        Destroy(gameObject);
+    }
---
+++ Assets/Scripts/Room5/ManagerRoom5.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerRoom5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int neededRightCollision = 5;
+
+    public GameObject ownRoom;
+    public List<GameObject> doors;
+    public GameObject bit0;
+    public GameObject bit1;
+    
+    private int currentRightCollision = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 spawnPosition = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
+        Vector3 spawnPosition2 = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
+        Rigidbody2D generatedBit = Instantiate(bit0, spawnPosition, Quaternion.identity).GetComponent<Rigidbody2D>();
+        Rigidbody2D generatedBit2 = Instantiate(bit1, spawnPosition2, Quaternion.identity).GetComponent<Rigidbody2D>();
+        generatedBit.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
+        generatedBit2.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
+    }
+
+    public void ApplyCollider(bool isGoodCollision)
+    {
+        if (isGoodCollision)
+        {
+            currentRightCollision++;
+        }
+        else currentRightCollision = 0;
+        if (currentRightCollision >= neededRightCollision)
+        {
+            foreach (GameObject door in doors)
+            {
+                Destroy(door);
+            }
+        }
+    }
+}
