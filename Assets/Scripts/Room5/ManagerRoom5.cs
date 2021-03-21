7a96c30eb87a45489e596c0ba4e515deb4ee0035
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2021-03-21 11:45:55 +0100

    update script

--- Assets/Scripts/Room5/ManagerRoom5.cs
+++ Assets/Scripts/Room5/ManagerRoom5.cs
@@ -11,18 +11,27 @@ public class ManagerRoom5 : MonoBehaviour
     public List<GameObject> doors;
     public GameObject bit0;
     public GameObject bit1;
+    private float lastUpdateTime;
     
     private int currentRightCollision = 0;
 
+    void Start()
+    {
+        lastUpdateTime = Time.time;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 spawnPosition = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
-        Vector3 spawnPosition2 = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
-        Rigidbody2D generatedBit = Instantiate(bit0, spawnPosition, Quaternion.identity).GetComponent<Rigidbody2D>();
-        Rigidbody2D generatedBit2 = Instantiate(bit1, spawnPosition2, Quaternion.identity).GetComponent<Rigidbody2D>();
-        generatedBit.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
-        generatedBit2.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
+        if (Time.time - lastUpdateTime > 1)
+        {
+            Vector3 spawnPosition = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
+            Vector3 spawnPosition2 = ownRoom.transform.position + new Vector3(Random.Range(-10,10), Random.Range(-10,10), 0);
+            Rigidbody2D generatedBit = Instantiate(bit0, spawnPosition, Quaternion.identity).GetComponent<Rigidbody2D>();
+            Rigidbody2D generatedBit2 = Instantiate(bit1, spawnPosition2, Quaternion.identity).GetComponent<Rigidbody2D>();
+            generatedBit.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
+            generatedBit2.velocity = new Vector3(Random.Range(-10f,10f), Random.Range(-10f,10f),0f);
+            lastUpdateTime = Time.time;
+        }
     }
 
     public void ApplyCollider(bool isGoodCollision)
