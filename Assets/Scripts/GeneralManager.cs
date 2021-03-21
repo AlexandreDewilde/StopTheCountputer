cd866e61ec74b8dd76258b18e64a8d0cc0903d32
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2021-03-21 02:50:39 +0100

    Add scripts for randoms

--- Assets/Scripts/GeneralManager.cs
+++ Assets/Scripts/GeneralManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GeneralManager : MonoBehaviour
 {
     public List<GameObject> rooms;
+    private List<Vector3> roomsCoords = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 originalCoords = new Vector3(0,0,0);
+        roomsCoords.Add(originalCoords);
     }
 
     // Update is called once per frame
@@ -23,6 +25,21 @@ public class GeneralManager : MonoBehaviour
         Vector3 distance = new Vector3(12.5f,12.5f,0f);
         distance.x = distance.x * direction.x;
         distance.y = distance.y * direction.y;
-        Instantiate(newRoom, position + distance, Quaternion.identity);
+        Vector3 roomPosition = distance + position;
+        if (!CheckIfCoordsExist(roomPosition))
+        {
+            Instantiate(newRoom, roomPosition, Quaternion.identity);
+            roomsCoords.Add(roomPosition);
+        }
+    }
+
+    bool CheckIfCoordsExist(Vector3 coords)
+    {
+        foreach (Vector3 roomCoord in roomsCoords)
+        {
+            if (roomCoord.x == coords.x && roomCoord.y == coords.y) return true;
+
+        }
+        return false;
     }
 }
