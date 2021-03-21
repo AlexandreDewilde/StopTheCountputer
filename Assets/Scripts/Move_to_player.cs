c84d82ede5bd4c44dfc007de298c1bf4f34d0f0f
Hippolyte Hilgers <hippolytehilgers@MacBook-Air-de-Hippolyte.local>
2021-03-21 02:35:38 +0100

    camera_move

---
+++ Assets/Scripts/Move_to_player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Move_to_player : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
